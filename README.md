5db7a4adb72ead87ba45870dbe523b73698ed333
Doston Nabotov <dostonnabotov@gmail.com>
2022-07-05 18:08:12 +0300

    full update

---
+++ README.md
@@ -0,0 +1,17 @@
+# Portfolio
+
+## References
+
+- [Design and Inspiration](https://brittanychiang.com/)
+
+- [some svg icons](https://frontend-mentor.io) from Frontend Mentor Challenges
+
+- [Responsive Font Sizes](https://www.youtube.com/watch?v=ykn4XNDwW7Q) from Kevin Powell
+- [Responsive Grid](https://www.youtube.com/watch?v=z2kuC7w9emE) from Kevin Powell
+
+- [Timeline Inspiration](https://www.w3schools.com/howto/howto_css_timeline.asp) from W3Schools
+- [Back to Top button](https://www.w3schools.com/howto/howto_js_scroll_to_top.asp) from W3Schools
+
+- [Breakpoints and Mixins](https://sass-guidelin.es/#responsive-web-design-and-breakpoints) from Sass Guidelines
+
+- [How to import JSON file into JavaScript](https://bobbyhadz.com/blog/javascript-import-json-file#:~:text=%20To%20import%20a%20JSON%20file%20in%20JavaScript%3A,myJson%20from%20%27.%2Fexample.json%27%20assert%20%7Btype%3A%20%27json%27%7D.%20More%20)
--- index.js
+++ index.js
@@ -1,65 +1,53 @@
-/* =========== Template =========== */
-const data = [
-  {
-    title: "Social Media Dashboard",
-    info: "Social Media Dashboard with theme switcher",
-    github: "https://github.com/dostonnabotov/social-media-dashboard-with-theme-switcher",
-    link: "https://dostonnabotov.github.io/social-media-dashboard-with-theme-switcher/",
-    img: "./assets/images/setup.jpg",
-  },
-  {
-    title: "Advice Generator App",
-    info: "Advice Generator App generates advice with help of API",
-    github: "https://github.com/dostonnabotov/advice-generator-app",
-    link: "https://dostonnabotov.github.io/advice-generator-app",
-    img: "./assets/images/setup.jpg",
-  },
-  {
-    title: "Basketball Scoreboard",
-    info: "Basketball Scoreboard keeps track of scores between HOME and GUEST players",
-    github: "https://github.com/dostonnabotov/basketball-scoreboard",
-    link: "https://dostonnabotov.github.io/basketball-scoreboard",
-    img: "./assets/images/setup.jpg",
-  }
-]
+/* =========== PROJECTS =========== */
+import data from './data.json' assert {type: 'json'};
+const projects = document.querySelector("[data-projects]");
 
-if ('content' in document.createElement('template')) {
-  var projects = document.querySelector("[data-projects]");
-  var template = document.querySelector("[data-project='template']");
+for (let i = 0; i < data.length; i++) {
+  const projectTemplate = `
+      <div class="project flow">
+        <div class="project__img">
+          <a href=${data[i].link} target="_blank" rel="noopener noreferrer">
+            <img src=${data[i].img} alt=${data[i].title} />
+          </a>
+        </div>
+        <div class="project__body flow">
+          <h3 class="fs-600 text-light">${data[i].title}</h3>
+          <p>${data[i].info}</p>
+        </div>
+        <div class="project__meta flex">
+          <a href=${data[i].github} target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-github"></i> Github
+          </a>
+          <a href=${data[i].link} target="_blank" rel="noopener noreferrer">
+            <i class="fas fa-link"></i> Live Site
+          </a>
+        </div>
+      </div>
+  `
 
-  for (let i = 0; i < data.length; i++) {
-    var clone = template.content.cloneNode(true);
-    const titleEl = clone.querySelector("[data-project='title']");
-    const githubEl = clone.querySelector("[data-project='github']");
-    const linkEl = clone.querySelectorAll("[data-project='link']");
-    const infoEl = clone.querySelector("[data-project='info']");
-    const imgEl = clone.querySelector("[data-project='img']");
-
-    titleEl.innerText = data[i].title;
-    infoEl.innerText = data[i].info;
-    githubEl.setAttribute("href", data[i].github);
-
-    setAttributes(imgEl, {
-      "src": data[i].img,
-      "alt": data[i].title,
-      "title": data[i].title
-    });
-    
-    linkEl.forEach(el => {
-      el.setAttribute("href", data[i].link);
-    });
-
-    // helper function to set several attributes at once
-    function setAttributes(el, attrs) {
-      for(var key in attrs) {
-        el.setAttribute(key, attrs[key]);
-      }
-    }
-    
-    projects.appendChild(clone);
-  }
-
-} else {
-// Find another way to add the rows to the table because
-// the HTML template element is not supported.
+  projects.innerHTML += projectTemplate;
 }
+
+/* =========== BACK TO TOP & NAVIGATION STYLE =========== */
+// Reference: https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+const backToTop = document.getElementById("backToTop");
+const headerEL = document.getElementById("header");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+    backToTop.style.display = "block";
+    headerEL.style.boxShadow = "0 1px 15px 2px hsl(0, 0%, 0%, .4)"
+  } else {
+    backToTop.style.display = "none";
+    headerEL.style.boxShadow = "none";
+  }
+}
+
+backToTop.addEventListener('click', () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+})
