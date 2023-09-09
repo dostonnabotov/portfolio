30f4f8960fce5c44b0da08e70b2947199ff0a4b5
Doston Nabotov <dostonnabotov@gmail.com>
2023-09-09 17:06:12 +0300

    Remove styles

--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,9 @@
 export interface PostProps {
   title: string;
-  description: string;
   url: string;
-  tag_list: string[];
   cover_image: string;
-  reading_time_minutes: number;
   created_at: Date;
   edited_at: Date;
-  comments_count: number;
-  public_reactions_count: number;
 }
 
 export interface ProjectProps {
