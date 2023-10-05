b74c18d39efe064810eba23c77322d614c8f20d8
Doston Nabotov <dostonnabotov@gmail.com>
2023-10-06 02:49:17 +0300

    Add skills section

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,14 +1,19 @@
-import type { PostProps, ProjectProps } from "../types";
+import type { ProjectProps, SkillProps } from "../types";
 import projects from "./projects.json";
+import skills from "./skills.json";
 
-export async function fetchBlogArticles(): Promise<PostProps[]> {
-  const response = await fetch(
-    "https://dev.to/api/articles?username=dostonnabotov"
-  );
-  const data = await response.json();
-  return data as PostProps[];
-}
+// export async function fetchBlogArticles(): Promise<PostProps[]> {
+//   const response = await fetch(
+//     "https://dev.to/api/articles?username=dostonnabotov"
+//   );
+//   const data = await response.json();
+//   return data as PostProps[];
+// }
 
 export function fetchProjects(limit?: number): ProjectProps[] {
   return limit ? projects.slice(0, limit) : projects;
+}
+
+export function fetchSkills(): SkillProps[] {
+  return skills;
 }
--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,19 @@
-export interface PostProps {
-  title: string;
-  url: string;
-  cover_image: string;
-  created_at: Date;
-  edited_at: Date;
-}
+// export interface PostProps {
+//   title: string;
+//   url: string;
+//   cover_image: string;
+//   created_at: Date;
+//   edited_at: Date;
+// }
 
 export interface ProjectProps {
   title: string;
   image: string;
   github_url: string;
   site_url: string;
+}
+
+export interface SkillProps {
+  name: string;
+  icon: string;
 }
